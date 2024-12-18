8dc9ce4930b2bb3c965c4bc396ad81533849cd94
Victor Aracil <victoraracil55@gmail.com>
2024-12-18 20:12:23 +0100

    voy haciendo cosas, primeros programas

---
+++ Java/Entornos/src/Cosopaborrar.java
@@ -0,0 +1,7 @@
+public class Cosopaborrar {
+    public static void main(String[] args) {
+        int number = 33;
+        System.out.printf("number is %05d", number); // number is 00033
+        System.out.printf("number is %5d", number);  // number is    33
+    }
+}
