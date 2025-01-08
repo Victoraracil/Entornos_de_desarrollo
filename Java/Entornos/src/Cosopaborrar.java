59e3037261d81581f163f98d835bd56376d91156
Victor Aracil <victoraracil55@gmail.com>
2025-01-08 20:13:41 +0100

    Ejercicios del primer punto de los apuntes acabados

--- Java/Entornos/src/Cosopaborrar.java
+++
@@ -1,7 +0,0 @@
-public class Cosopaborrar {
-    public static void main(String[] args) {
-        int number = 33;
-        System.out.printf("number is %05d", number); // number is 00033
-        System.out.printf("number is %5d", number);  // number is    33
-    }
-}
