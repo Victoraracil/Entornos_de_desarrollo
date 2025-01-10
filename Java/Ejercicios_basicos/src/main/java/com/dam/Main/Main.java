5ef862a95be8a9edc159bd383b934137891f062a
Victor Aracil <victoraracil55@gmail.com>
2025-01-10 15:57:23 +0100

    Nuevo proyecto para guardar todo entornos de desarrollo

---
+++ Java/Ejercicios_basicos/src/main/java/com/dam/Main/Main.java
@@ -0,0 +1,14 @@
+package com.dam.Main;
+
+import com.dam.ejercicios.Basicos;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        //Basicos.formattedDate();
+        //Basicos.GramOunceConverter();
+        //Basicos.NumbersStrings();
+        Basicos.CircleArea();
+    }
+}
